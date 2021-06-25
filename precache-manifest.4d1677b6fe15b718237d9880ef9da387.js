ffedbdce08cc60398aec426c813002a817779854
chaimat <f20190512@goa.bits-pilani.ac.in>
2021-06-25 13:59:40 +0530

    Updates

---
+++ precache-manifest.4d1677b6fe15b718237d9880ef9da387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947ecdb3f543433d59af2b803b0f8b5e",
+    "url": "/youtube-mock/index.html"
+  },
+  {
+    "revision": "2ecb3f0813a2842b2393",
+    "url": "/youtube-mock/static/css/main.4965ec2a.chunk.css"
+  },
+  {
+    "revision": "b0cce0658d94e0178606",
+    "url": "/youtube-mock/static/js/2.dc9adb55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-mock/static/js/2.dc9adb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ecb3f0813a2842b2393",
+    "url": "/youtube-mock/static/js/main.c3f4356b.chunk.js"
+  },
+  {
+    "revision": "9a3eb6884f7a8a51854d",
+    "url": "/youtube-mock/static/js/runtime-main.2a3379c9.js"
+  }
+]);
